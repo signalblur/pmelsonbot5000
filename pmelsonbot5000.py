6df66053e447adeda37209a0c1a6bf5f727e5a38
David Burkett <45216760+david-burkett@users.noreply.github.com>
2019-05-12 23:50:26 -0400

    Basic error handling/commenting

--- pmelsonbot5000.py
+++ pmelsonbot5000.py
@@ -5,21 +5,12 @@ import tweepy
 import yaml
 import re
 import csv
+import time
 from datetime import datetime, timedelta
 from github import Github
 
 start_date = datetime.today() - timedelta(days=1)
 end_date = datetime.today()
-
-def git_manager(access_token):
-    """
-    git_manager serves to perform all updates to the github repo itself.
-    """
-
-    git = Github("access_token")
-
-    repo = git.get_repo("david-burkett/pmelsonbot5000")
-    contents = repo.get_contents("README.md", ref="test")
 
 def ioc_csv(ioc_data):
     """
@@ -30,11 +21,15 @@ def ioc_csv(ioc_data):
 
     top_row = ['date_identified', 'malware/attack_tool', 'url', 'hash', 'c2']
 
-    with open('scumfeed.csv', 'a') as fp:
-        wr = csv.writer(fp)
-        wr.writerow(top_row)
-        for list in ioc_data:
-            wr.writerow(list)
+    try:
+        with open('scumfeed.csv', 'a') as fp:
+            wr = csv.writer(fp)
+            wr.writerow(top_row)
+            for list in ioc_data:
+                wr.writerow(list)
+    except Exception as e:
+        print('CSV creator failure.')
+        print(e)
             
 
 def extract(data):
@@ -45,37 +40,55 @@ def extract(data):
     ioc_list = []
     pattern = '((?P<name>\S{1,}) found at (?P<url>\S{1,}) SHA256: (?P<hash>[a-zA-Z0-9]{1,}) C2: (?P<c2>\S{1,}))'
     
-    for tweet in data:
-        append_list = []
-        if str(tweet.created_at) >= str(start_date.strftime("%Y-%m-%d %H:%M:%S")):
-            reg_data = re.search(pattern, str(tweet.full_text))
-            append_list.append(tweet.created_at)
-            append_list.append(reg_data.group('name'))
-            append_list.append(reg_data.group('url'))
-            append_list.append(reg_data.group('hash'))
-            append_list.append(reg_data.group('c2'))
-            ioc_list.append(append_list)
+    try:
+        for tweet in data:
+            append_list = []
+            if str(tweet.created_at) >= str(start_date.strftime("%Y-%m-%d %H:%M:%S")):
+                reg_data = re.search(pattern, str(tweet.full_text))
+                append_list.append(tweet.created_at)
+                append_list.append(reg_data.group('name'))
+                append_list.append(reg_data.group('url'))
+                append_list.append(reg_data.group('hash'))
+                append_list.append(reg_data.group('c2'))
+                ioc_list.append(append_list)
+    except Exception as e:
+        print('Regex Parser failer')
+        print(e)
     
     return ioc_list
     
 
 def main():
-
-    with open('conf.yaml', 'r') as f:
-        conf_data = yaml.safe_load(f)
-
-    auth = tweepy.OAuthHandler(conf_data['consumer_key'], conf_data['consumer_secret'])
-    auth.set_access_token(conf_data['access_token'], conf_data['access_token_secret'])
-
-    api = tweepy.API(auth)
-
-    twitter_raw = tweepy.Cursor(api.user_timeline, id="scumbots", since=start_date, until=end_date, tweet_mode='extended').items()
+    """
+    App grabs past 30 days from the @ScumBots twitter feed, parses
+    the data and makes it easily parsed for common security tooling.
+    """
     
-    twitter_formatted = extract(twitter_raw)
-    
-    csv_twitter_formatted = ioc_csv(twitter_formatted)
+    while True:
+        try: # Authentication area.
+            with open('conf.yaml', 'r') as f:
+                conf_data = yaml.safe_load(f)
 
-    deploy = git_manager(conf_data['git_token'])
+            auth = tweepy.OAuthHandler(conf_data['consumer_key'], conf_data['consumer_secret'])
+            auth.set_access_token(conf_data['access_token'], conf_data['access_token_secret'])
+
+            api = tweepy.API(auth)
+
+        except Exception as e:
+            print('Unable to authenticate.')
+            print(e)
+
+        try: # Data Collection and parsing
+            twitter_raw = tweepy.Cursor(api.user_timeline, id="scumbots", since=start_date, until=end_date, tweet_mode='extended').items()
+
+            twitter_formatted = extract(twitter_raw)
+
+            csv_twitter_formatted = ioc_csv(twitter_formatted)
+            time.sleep(180) # Sleeping for 24 hours
+
+        except Exception as e:
+            print('Data parsing error')
+            print(e)
 
 
 if __name__ == "__main__":
